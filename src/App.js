ce3c37eee9b575960b267072e970381a4a7c0347
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-04-27 14:16:04 +0100

    Map cities to timezones

--- src/App.js
+++ src/App.js
@@ -1,3 +1,70 @@
+const CITIES = [
+  {
+    name: 'Moscow',
+    gmt: 3,
+  },
+  {
+    name: 'Paris',
+    gmt: 2,
+  },
+  {
+    name: 'Berlin',
+    gmt: 2,
+  },
+  {
+    name: 'Brussels',
+    gmt: 2,
+  },
+  {
+    name: 'Amsterdam',
+    gmt: 2,
+  },
+  {
+    name: 'Rome',
+    gmt: 2,
+  },
+  {
+    name: 'London',
+    gmt: 1,
+  },
+  {
+    name: 'Dublin',
+    gmt: 1,
+  },
+  {
+    name: 'New York',
+    gmt: -4,
+  },
+  {
+    name: 'Washington, DC',
+    gmt: -4,
+  },
+  {
+    name: 'St. Louis',
+    gmt: -5,
+  },
+  {
+    name: 'Los Angeles',
+    gmt: -7,
+  },
+  {
+    name: 'Tokyo',
+    gmt: 9,
+  },
+  {
+    name: 'Moscow',
+    gmt: 8,
+  },
+  {
+    name: 'Moscow',
+    gmt: 7,
+  },
+  {
+    name: 'Moscow',
+    gmt: 5,
+  },
+];
+
 function App() {
   return <h1>hello</h1>;
 }
